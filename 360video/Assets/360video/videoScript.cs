7dd3cff769410572a078a8ef9d0144d181e0db31
Unknown <hmtai6@student.cityu.edu.hk>
2017-03-20 12:32:04 +0800

    update code

---
+++ 360video/Assets/360video/videoScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class videoScript : MonoBehaviour {
+	public bool loop = true;
+	public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+		movie.Play ();
+		movie.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
